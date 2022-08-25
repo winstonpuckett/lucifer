f53a451826781d18bae211e72953166f571e74f6
Bobby Puckett <puckett.bobby7@gmail.com>
2022-08-24 21:28:48 -0700

    Call a command

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
+use std::process::Command;
+use std::str;
 use std::env;
+
+// To test:
+// cargo run -- /C echo hello, bobby
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    for arg in args {
-        println!("{arg}");
-    }
+    let output = Command::new("cmd")
+        .args(args)
+        .output()
+        .expect("Could not call tool");
+    
+    let code = output.status;
+    println!("{code}");
+    
+    let stdout = str::from_utf8(&output.stdout).unwrap();
+    print!("stdout: {stdout}");
+    
+    let stderr = str::from_utf8(&output.stderr).unwrap();
+    print!("stderr: {stderr}");
 }
