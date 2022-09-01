e9bb62e2171d88b4922dba57283a4d32b5eb8d1c
Bobby Puckett <puckett.bobby7@gmail.com>
2022-08-31 20:04:22 -0700

    Design output

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,3 @@ fn main() {
     let suite = constructor::construct("./test_self").unwrap();
     executor::execute(suite);
 }
-
-
-// TODO: Move this to a env module or something.
-// To test:
-// cargo run -- /C echo hello, bobby
-// use std::process::Command;
-// use std::str;
-// use std::env;
-// let args: Vec<String> = env::args().collect();
-// let output = Command::new("cmd")
-//     .args(args)
-//     .output()
-//     .expect("Could not call tool");
-// let code = output.status;
-// println!("{code}");
-// let stdout = str::from_utf8(&output.stdout).unwrap();
-// print!("stdout: {stdout}");
-// let stderr = str::from_utf8(&output.stderr).unwrap();
-// print!("stderr: {stderr}");
