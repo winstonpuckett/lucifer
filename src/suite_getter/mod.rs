601176a5466342ead04ae15084bc95d1c18aa5d7
Winston Puckett <puckett.bobby7@gmail.com>
2022-10-30 17:52:18 +0000

    Refactor file handling

--- src/suite_getter/mod.rs
+++ src/suite_getter/mod.rs
@@ -1,51 +1,41 @@
-use std::{fs, path::Path};
+use std::{
+    fs::{self, DirEntry},
+    io::Error,
+    path::Path,
+};
 extern crate yaml_rust;
-use crate::{args_getter::Args, ExitCode, CommandResult};
-use self::{transformer::{Serialization, Feature}, sorter::is_lucifer_file};
-mod transformer;
+use self::{
+    sorter::is_lucifer_file,
+    transformer::{Feature, Serialization},
+};
+use crate::{args_getter::Args, CommandResult, ExitCode};
 mod sorter;
+mod transformer;
 
 pub fn get_suite(args: Args) -> CommandResult<Suite> {
     let path = Path::new(&args.input_directory);
     if path.is_dir() {
-        // TODO: Catch errors from unwrapping folders.
         let files = fs::read_dir(&args.input_directory).unwrap();
-        let mut features: Vec<Feature> = vec![];
 
-        for file_result in files {
-            if file_result.is_err() {
-                // TODO: Figure out why a file would be in error.
-                continue;
-            }
-            
-            let file = file_result.unwrap();
-            
-            if !is_lucifer_file(&file) {
-                continue;
-            }
+        let features = files
+            .map(to_feature_option)
+            .filter(|f| f.is_some())
+            .map(|f| f.unwrap())
+            .collect();
 
-            features.push(transformer::to_feature(&file));
-        }
-
-        let suite = Suite {
-            args,
-            features
-        };
+        let suite = Suite { args, features };
 
         Ok(suite)
     } else if path.is_file() {
         let file_result = fs::read_to_string(&args.input_directory);
-        let mut features: Vec<Feature> = vec![];
-
-            
         let file = file_result.unwrap();
 
-        features.push(transformer::to_feature_from_str(path.file_name().unwrap().to_str().unwrap(), &file));
+        let features = vec![transformer::to_feature_from_str(
+            path.file_name().unwrap().to_str().unwrap(),
+            &file,
+        )];
 
-        let suite = Suite {
-            args,
-            features
-        };
+        let suite = Suite { args, features };
 
         Ok(suite)
     } else {
@@ -53,9 +43,23 @@ pub fn get_suite(args: Args) -> CommandResult<Suite> {
     }
 }
 
+fn to_feature_option(f: Result<DirEntry, Error>) -> Option<Feature> {
+    if f.is_err() {
+        return None;
+    }
+
+    let file = f.unwrap();
+
+    if !is_lucifer_file(&file) {
+        return None;
+    }
+
+    Some(transformer::to_feature(&file))
+}
+
 pub struct Suite {
     pub args: Args,
-    pub features: Vec<transformer::Feature>
+    pub features: Vec<transformer::Feature>,
 }
 
 pub struct Test {
@@ -63,7 +67,7 @@ pub struct Test {
     pub description: String,
     pub expectations: Expectations,
     pub name: String,
-    pub serialization: Serialization
+    pub serialization: Serialization,
 }
 
 #[derive(Clone)]
@@ -74,5 +78,5 @@ pub struct Expectations {
     pub error: Option<String>,
     pub no_file: Option<String>,
     pub file: Option<String>,
-    pub contents: Option<String>
+    pub contents: Option<String>,
 }
