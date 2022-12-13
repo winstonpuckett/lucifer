0b6578690e71b778600a16f1740e8491ec8f3088
Winston Puckett <puckett.bobby7@gmail.com>
2022-12-13 04:46:36 +0000

    refactor result mutator

--- src/args_getter/result_mutator.rs
+++ src/args_getter/result_mutator.rs
@@ -4,53 +4,140 @@ use crate::ExitCode;
 
 use super::{Args, RunMode};
 
-pub fn mutate(result: &mut Args, current_arg: String, args: &mut std::iter::Peekable<env::Args>) -> Result<(), (ExitCode, Option<String>)> {
-    if is_help_command(&current_arg) {
-        result.run_mode = RunMode::Help;
-    } else if is_version_command(&current_arg) {
-        result.run_mode = RunMode::Version;
-    } else if is_silent_flag(&current_arg) {
-        result.console_silent = true;
-    } else if is_no_file_flag(&current_arg) {
-        result.no_file = true;
-    } else if is_input_directory(&current_arg) {
-        if args.peek().is_none() {
-            return Err((ExitCode::UserError, Some(format!("Expected an input directory after {:?}, but none was provided.", current_arg))));
-        }
-            
-        result.input_directory = args.next().unwrap();
-    } else if is_output_directory(&current_arg) {
-        if args.peek().is_none() {
-            return Err((ExitCode::UserError, Some(format!("Expected an output directory after {:?}, but none was provided.", current_arg))));
-        }
+pub fn mutate(
+    result: &mut Args,
+    current_arg: String,
+    args: &mut std::iter::Peekable<env::Args>,
+) -> Result<(), (ExitCode, Option<String>)> {
+    mutate_flags(&current_arg, result);
 
-        result.output_directory = args.next().unwrap();
+    return mutate_options(current_arg, result, args);
+}
+
+fn mutate_options(current_arg: String, result: &mut Args, args: &mut std::iter::Peekable<env::Args>) -> Result<(), (ExitCode, Option<String>)> {
+    let options_parsers = get_options();
+    let options = options_parsers
+        .iter()
+        .filter(|f| {
+            f.name_short.eq_ignore_ascii_case(&current_arg)
+                || f.name_long.eq_ignore_ascii_case(&current_arg)
+        });
+
+    for o in options {
+        let r = (o.mutator)(result, &current_arg, args);
+        
+        if r.is_err() {
+            return r;
+        }
     }
 
     Ok(())
 }
 
-fn is_help_command(arg: &String) -> bool {
-    arg.eq_ignore_ascii_case("-h")
-    || arg.eq_ignore_ascii_case("--help")
+fn get_options() -> Vec<OptionParser> {
+    vec![
+        OptionParser {
+            name_short: String::from("-i"),
+            name_long: String::from("--input-directory"),
+            mutator: |result: &mut Args,
+                      current_arg: &String,
+                      args: &mut std::iter::Peekable<env::Args>| {
+                if args.peek().is_none() {
+                    return Err((
+                        ExitCode::UserError,
+                        Some(format!(
+                            "Expected an input directory after {:?}, but none was provided.",
+                            current_arg
+                        )),
+                    ));
+                }
+
+                result.input_directory = args.next().unwrap();
+
+                Ok(())
+            },
+        },
+        OptionParser {
+            name_short: String::from("-o"),
+            name_long: String::from("--output-directory"),
+            mutator: |result: &mut Args,
+                      current_arg: &String,
+                      args: &mut std::iter::Peekable<env::Args>| {
+                if args.peek().is_none() {
+                    return Err((
+                        ExitCode::UserError,
+                        Some(format!(
+                            "Expected an output directory after {:?}, but none was provided.",
+                            current_arg
+                        )),
+                    ));
+                }
+
+                result.output_directory = args.next().unwrap();
+
+                Ok(())
+            },
+        },
+    ]
 }
-fn is_version_command(arg: &String) -> bool {
-    arg.eq_ignore_ascii_case("-v")
-    || arg.eq_ignore_ascii_case("--version")
+
+fn mutate_flags(current_arg: &String, result: &mut Args) {
+    get_flags()
+        .iter()
+        .filter(|f| {
+            f.name_short.eq_ignore_ascii_case(current_arg)
+                || f.name_long.eq_ignore_ascii_case(current_arg)
+        })
+        .for_each(|f| {
+            (f.mutator)(result);
+        });
 }
-fn is_silent_flag(arg: &String) -> bool {
-    arg.eq_ignore_ascii_case("-s")
-    || arg.eq_ignore_ascii_case("--silent")
+
+fn get_flags() -> Vec<FlagParser> {
+    vec![
+        FlagParser {
+            name_short: String::from("-h"),
+            name_long: String::from("--help"),
+            mutator: |result: &mut Args| {
+                result.run_mode = RunMode::Help;
+            },
+        },
+        FlagParser {
+            name_short: String::from("-v"),
+            name_long: String::from("--version"),
+            mutator: |result: &mut Args| {
+                result.run_mode = RunMode::Version;
+            },
+        },
+        FlagParser {
+            name_short: String::from("-s"),
+            name_long: String::from("--silent"),
+            mutator: |result: &mut Args| {
+                result.console_silent = true;
+            },
+        },
+        FlagParser {
+            name_short: String::from("-n"),
+            name_long: String::from("--no-file"),
+            mutator: |result: &mut Args| {
+                result.no_file = true;
+            },
+        },
+    ]
 }
-fn is_no_file_flag(arg: &String) -> bool {
-    arg.eq_ignore_ascii_case("-n")
-    || arg.eq_ignore_ascii_case("--no-file")
+
+struct FlagParser {
+    name_short: String,
+    name_long: String,
+    mutator: fn(result: &mut Args),
 }
-fn is_input_directory(arg: &String) -> bool {
-    arg.eq_ignore_ascii_case("-i")
-    || arg.eq_ignore_ascii_case("--input-directory")
-}
-fn is_output_directory(arg: &String) -> bool {
-    arg.eq_ignore_ascii_case("-o")
-    || arg.eq_ignore_ascii_case("--output-directory")
+
+struct OptionParser {
+    name_short: String,
+    name_long: String,
+    mutator: fn(
+        result: &mut Args,
+        current_arg: &String,
+        args: &mut std::iter::Peekable<env::Args>,
+    ) -> Result<(), (ExitCode, Option<String>)>,
 }
